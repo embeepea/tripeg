502983b8f82c008d1ead1275747cd0abdae8d4e8
Mark Phillips <embeepea@git>
2014-01-03 19:16:24 -0500

    ignore canvas mouse move events unless game is in `start` state

--- tripeg-ui.js
+++ tripeg-ui.js
@@ -1,5 +1,7 @@
 (function($) {
 
+    var peg_click_allowed = true;
+
     function click_peg_to_emtpy_message() {
         $('#message').html("Click to change the initial empty position");
     }
@@ -43,20 +45,18 @@
 
         $('#play').click(function() {
             var $icon = $(this).find('i');
-            //$icon.removeClass('fa-play');
-            //$icon.addClass('fa-pause');
             clear_message();
             $('#play').prop('disabled', true);
+            peg_click_allowed = false;
             tripeg.play(
                 function() {
-                    //$icon.removeClass('fa-pause');
-                    //$icon.addClass('fa-play');
                     $('#rotate-left').prop('disabled', false);
                 },
                 function() {
                     splash("No solution");
                     $('#rotate-left').prop('disabled', false);
                     $('#play').prop('disabled', false);
+                    peg_click_allowed = true;
                 }
             );
         });
@@ -64,6 +64,7 @@
             tripeg.reset();
             click_peg_to_emtpy_message();
             $('#play').prop('disabled', false);
+            peg_click_allowed = true;
         }).prop('disabled', true);
 
         $('#plus').click(function() {
@@ -78,36 +79,36 @@
 
         $('#thecanvas').click(function() {
             if (highlighted_peg !== undefined) {
+                highlighted_peg.highlight(false);
+                highlighted_peg = undefined;
                 tripeg.moveToEmpty(highlighted_pos);
             }
         });
 
         $('#thecanvas').mousemove(function(event) {
-            //console.log(event.clientX + ', ' + event.clientY);
-            var peg_is_highlighted = false;
-            if (tripeg.point_in_triangle(event.offsetX, event.offsetY)) {
-                var p = tripeg.point_in_peg(event.offsetX, event.offsetY);
-                if (p) {
-                    peg_is_highlighted = true;
-                    highlighted_pos = p;
-                    highlighted_peg = tripeg.board.get_peg(p[0], p[1]);
-                    if (!highlighted_peg.highlighted) {
-                        highlighted_peg.highlight(true);
-                        $('#thecanvas').css('cursor', 'pointer');
-                        tripeg.request_draw();
-                        return;
+            if (peg_click_allowed) {
+                var peg_is_highlighted = false;
+                if (tripeg.point_in_triangle(event.offsetX, event.offsetY)) {
+                    var p = tripeg.point_in_peg(event.offsetX, event.offsetY);
+                    if (p) {
+                        peg_is_highlighted = true;
+                        highlighted_pos = p;
+                        highlighted_peg = tripeg.board.get_peg(p[0], p[1]);
+                        if (!highlighted_peg.highlighted) {
+                            highlighted_peg.highlight(true);
+                            $('#thecanvas').css('cursor', 'pointer');
+                            tripeg.request_draw();
+                            return;
+                        }
                     }
-                    //console.log(p);
                 }
-                //console.log(event.offsetX + ', ' + event.offsetY);
+                if (!peg_is_highlighted && highlighted_peg !== undefined) {
+                    highlighted_peg.highlight(false);
+                    highlighted_peg = undefined;
+                    $('#thecanvas').css('cursor', 'default');
+                    tripeg.request_draw();
+                }
             }
-            if (!peg_is_highlighted && highlighted_peg !== undefined) {
-                highlighted_peg.highlight(false);
-                highlighted_peg = undefined;
-                $('#thecanvas').css('cursor', 'default');
-                tripeg.request_draw();
-            }
-            //console.log(event);
         });
 
     });
